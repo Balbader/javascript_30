4bedf40c716d422947e1c24e65b885adb893a39b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-16 14:22:56 +0200

    update

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -7,15 +7,18 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55'; // need a color when you draw a line
 ctx.lineJoin = 'round'; // end of the line will be rounded around the corner
 ctx.lineCap = 'round'; // end of the line will be rounded around the corner
+ctx.lineWidth = 50; // width of the line drawn
 
 let isDrawing = false; // when click on canvas, draw then let go.. the mouse will not draw anymore
 let lastX = 0; // where you start the line from
 let lastY = 0; // where you end the lind
-
-function draw(e) {
+let hue = 0; // variable to change the color of the line
+const draw = (e) => {
     if (!isDrawing)
         return; // stop the function from running when they are not moused down
     console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; // setting up the color change for the line. It starts red.
+
     /* ------ The drawing part ------ */
     ctx.beginPath();
     // start from 
@@ -24,6 +27,7 @@ function draw(e) {
     ctx.lineTo(e.offsetX, e.offsetY); // fetch the values of the actual event happening on the canvas
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++; // increment value of color to go through the entire spectrum
 }
 
 /* ----------- action to trigger the drawing ----------------- */
@@ -34,4 +38,3 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false); // make sure to tell that the mouse is not drawing anymore
-
