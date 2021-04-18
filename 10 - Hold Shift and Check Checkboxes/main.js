6fecd91a1dd9aca3aae9aa51b61f2c91b75eba4d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-18 18:19:15 +0200

    update

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,6 +1,21 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-    console.log(e);
+    // Check if they have the shit key down
+    // And check that they are checking it
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        // go ahead and do what we please
+        // loop over every single checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+        });
+    }
+    lastChecked = this;
 }
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
