693a6ec5f836ec681fbb584361471416bb0708d5
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-19 20:34:46 +0200

    update

--- 11 - Custom Video Player/main.js
+++ 11 - Custom Video Player/main.js
@@ -10,13 +10,22 @@ const ranges = player.querySelectorAll('.player__slider');
 
 /* Build our functions */
 const togglePlay = () => {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    /*
     if (video.paused) {
         video.play();
     } else {
         video.pause();
     }
+    */
 }
 
+const updateButton = () => {
+    console.log('update the button');
+}
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+toggle.addEventListener('click', togglePlay);
