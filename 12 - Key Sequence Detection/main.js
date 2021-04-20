f8a62116c8ae23e0456502dcd6e1b574685a8ad9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-20 11:35:26 +0200

    update

--- 12 - Key Sequence Detection/main.js
+++ 12 - Key Sequence Detection/main.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const pressed = [];
+const secretCode = 'balou';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    // 1. Record the key stroke into an array
+    pressed.push(e.key);
+    
+    //2. Trim down the array to match the length of secret code
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    // 3. Join the array into a string
+    if (pressed.join('').includes(secretCode)) {
+        console.log('Secret Code Found!!');
+        cornify_add(); // Makes stickers appear on screen when secret code is found
+    }
+    console.log(pressed);
+});
