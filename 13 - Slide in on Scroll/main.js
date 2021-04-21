6e613a57a536201faa953d26e8e7f3f7ba7be2ea
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-21 17:51:24 +0200

    update

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -15,6 +15,10 @@ function debounce(func, wait = 20, immediate = true) {
 
 const sliderImages = document.querySelectorAll('.slide-in');
 const checkSlide = (e) => {
-    console.count(e);
+    sliderImages.forEach(slideImage => {
+        // figure out how far is the page scroll down
+        const slideInAt = (window.screenY + window.innerHeight) - slideImage.height / 2;
+        console.log(slideInAt);
+    });
 }
-window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener('scroll', debounce(checkSlide)); // Always debounce scroll functions
