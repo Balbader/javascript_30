1bdcf41dac2ece21475760f3ed6ee8d0e368d984
Balbader <15971200+Balbader@users.noreply.github.com>
2021-04-21 18:04:16 +0200

    update

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -15,13 +15,20 @@ function debounce(func, wait = 20, immediate = true) {
 
 const sliderImages = document.querySelectorAll('.slide-in');
 const checkSlide = (e) => {
-    sliderImages.forEach(slideImage => {
+    sliderImages.forEach(sliderImage => {
         // figure out how far is the page scroll down
         //
         // 1/2 way through the image
-        const slideInAt = (window.screenY + window.innerHeight) - slideImage.height / 2;
+        const slideInAt = (window.screenY + window.innerHeight) - sliderImage.height / 2;
         // bottom of the image
-        const imageBottom = sliderImage.offsetTop + slideImage.height;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
     });
 }
 window.addEventListener('scroll', debounce(checkSlide)); // Always debounce scroll functions
