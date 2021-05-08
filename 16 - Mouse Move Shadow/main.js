12c11310f961a7b5108ad9786f677f19bc1333b3
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-08 15:41:10 +0200

    update

--- 16 - Mouse Move Shadow/main.js
+++ 16 - Mouse Move Shadow/main.js
@@ -15,6 +15,7 @@ function shadow(e) {
     const xWalk = Math.round((x / width * walk) - (walk / 2))
     const yWalk = Math.round((y / height * walk) - (walk / 2))
 
+    // Setting up the text shadow
     text.style.textShadow = `
       ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
       ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
