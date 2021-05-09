3fa8c14928cd488dada6f0755fcbad7cf8a68c58
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-09 16:13:16 +0200

    update

--- 18 - Adding Up Times with Reduce/main.js
+++ 18 - Adding Up Times with Reduce/main.js
@@ -2,3 +2,12 @@
 // add them all up, figure out the total amount of hrs, min, seconds
 // and console.log the result
 
+// turn the node-list into an array of node strings
+const timeNodes = Array.from(document.querySelectorAll('[data-time'));
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':');
+        console.log(mins, secs);
+    })
