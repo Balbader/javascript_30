a8b504a166d055b9717ca3d344c38bbc07eda091
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-10 14:08:18 +0200

    update

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,30 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo () {
+    // Get video from user device
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+
+            //  DEPRECIATION : 
+//       The following has been deprecated by major browsers as of Chrome and Firefox.
+//       video.src = window.URL.createObjectURL(localMediaStream);
+//       Please refer to these:
+//       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+            video.srcObject = localMediaStream;
+            video.play();
+    })
+    .catch(err => {
+        console.log(`ERROR!!!`, err);
+    });
+}
+
+function paintToCanvas () {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+}
+
+getVideo();
