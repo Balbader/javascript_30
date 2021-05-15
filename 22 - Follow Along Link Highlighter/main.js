741b60c76a88655dd5bb112b1f781135b481b48e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-15 18:51:11 +0200

    update

--- 22 - Follow Along Link Highlighter/main.js
+++ 22 - Follow Along Link Highlighter/main.js
@@ -6,6 +6,9 @@ document.body.append(highlight);
 function highlightLink() {
     const linkCoords = this.getBoundingClientRect(); // Will print all the info about where on the page the item is located
     console.log(linkCoords);
+    // applying style to the element
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
